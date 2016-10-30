8e572c7bb60f928f0317bef6447dd15a1b232d55
jrn2477 <jrn2477@rit.edu>
2016-10-30 17:16:34 -0400

    Added some GUI Components

--- GUI.java
+++ GUI.java
@@ -4,10 +4,18 @@ import java.awt.*;
 public class GUI extends JFrame {
 	
 	JFrame frame; 
-	JPanel mainPanel; 
+	JMenu fileMenu; 
+	JMenuItem helpMenuItem, exitMenuItem; 
+	JMenuBar menuBar; 
+	JPanel mainPanel, southPanel, northPanel, connectionPanel; 
 	JButton sendMessageButton, connectButton;
-	JTextField ipTextField, targetPortTextField, recievePortTextField; 
-	JTextArea chatArea; 
+	JLabel nameFieldLabel, ipAddressFieldLabel, portNumFieldLabel, messageLabel; 
+	JTextField ipAddressField, nameField, portNumField; 
+	JTextArea chatArea;  
+	
+	public static void main(String[] args) {
+		GUI g1 = new GUI(); 
+	}
 	
 	public GUI() {
 		
@@ -17,15 +25,34 @@ public class GUI extends JFrame {
 	/*
 	* Method to intitialize components 
 	*/ 
-	public init(){
+	public void init(){
 		
 		frame = new JFrame(); 
-		mainPanel = new Panel(new BorderLayout());
-		sendMessageButton = new JButton("Send Message"); 
-			// ADD ACTIONLISTINER 
-		connectButton = new JButton("Connect"); 
-			// ADD ACTIONLISTENER 
 		
+		mainPanel = new JPanel(new BorderLayout());
+		northPanel = new JPanel(new GridLayout(1,2));
+		connectionPanel = new JPanel(new GridLayout(3,2));
+		southPanel = new JPanel(new GridLayout(1,2));
+		
+		sendMessageButton = new JButton("Send Message");
+		sendMessageButton.enable(false);
+			// TODO: Add actionListener 
+		connectButton = new JButton("Connect"); 
+			// TODO: Add actionListener 
+		
+		helpMenuItem = new JMenuItem("Help");
+			// TODO: Add actionListener 
+		exitMenuItem = new JMenuItem("Exit");
+			// TODO: Add actionListener 
+		
+		nameFieldLabel = new JLabel("Name: "); 
+		ipAddressFieldLabel = new JLabel("IP Address"); 
+		portNumFieldLabel = new JLabel("Port Number:"); 
+		messageLabel = new JLabel("Message:");
+		
+		ipAddressField = new JTextField(15); 
+		nameField = new JTextField(15);
+		portNumField = new JTextField(5); 
 		
 		/*
 			CODE TO SETUP LAYOUT 
